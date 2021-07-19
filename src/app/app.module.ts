6d9f5b73bf6ae339c46a0d28f524487f59c30d90
Kirui sang <brian.kirui@student.moringaschool.com>
2021-07-19 03:03:52 +0300

    final commit of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from 'angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotedComponent } from './quoted/quoted.component';
+import { QuotedDetailsComponent } from './quoted-details/quoted-details.component';
+import { QuotedFormsComponent } from './quoted-forms/quoted-forms.component';
+import { DatePipe } from './date.pipe';
+import { HighlightQuoteDirective } from './highlight-quote.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotedComponent,
+    QuotedDetailsComponent,
+    QuotedFormsComponent,
+    DatePipe,
+    HighlightQuoteDirective
   ],
   imports: [
     BrowserModule,

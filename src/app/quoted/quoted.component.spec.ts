f03d0e9331b418ecbd7ec876ee6d5ff5ee1719ca
Kirui sang <brian.kirui@student.moringaschool.com>
2021-07-19 03:02:08 +0300

    updated the quotes

---
+++ src/app/quoted/quoted.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuotedComponent } from './quoted.component';
+
+describe('QuotedComponent', () => {
+  let component: QuotedComponent;
+  let fixture: ComponentFixture<QuotedComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ QuotedComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuotedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/quoted/quoted.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes'
+
+@Component({
+  selector: 'app-quoted',
+  templateUrl: './quoted.component.html',
+  styleUrls: ['./quoted.component.css']
+})
+export class QuotedComponent implements OnInit {
+    title = 'Welcome to your world of quotes'
+    quoted: Quotes[] = [
+      new Quotes (1, 'Faith', 'Deep','The way I see it, if you  want the rainbow, you have to put up with the rain.','Dolly  Parton', new Date(2021,7,19),0,0),
+      new Quotes (2,'Ilkay','Inspiration','If you want to fly   give up everything that weighs you down.','Gurubogsa',new   Date(2021,7,20),0,0),
+      new Quotes (3, 'Marion','Love','Sometimes you will never  know the value of a moment until it becomes a memory.','DR   SEUSS',new Date(2021,7,18),0,0),
+      new Quotes (4, 'Barbosa','Positive','When life gives you a  hundred reasons to break down and cry,show life that you   have a million reasons to smile and laugh.','FunZmo',new  Date(2021,7,19),0,0),
+      new Quotes (5, 'Kiyoko','Philosophical','The way we see the   problem is the problem.','Stephen Covey',new Date(2021,7,21)  ,0,0),
+    ];
+  get sortQuotes(){
+    return this.quoted.sort((a, b) => {
+      return <any>new Date(.datePosted) - <any>new Date(a.  datePosted);
+    });
+  }
+  get sortQuotes() {
+    return this.quoted.sort((a, b) => {
+      return <any>new Date(b.datePosted) - <any>new Date(a. datePosted);
+    });
+  }
+  addedQuote(quotes){
+    let arraysize = this.quoted.length;
+    quotes.id = arraysize+1;
+    quotes.completeDate = new Date(quotes.completeDate)
+    this.quoted.push(quotes)
+  } 
+  quoteDelete(isRead, index){
+    if (isRead) {
+      let toDelete = confirm(`Are you sure you want to delete this Quote?`)
+      if(toDelete){
+        this.quoted.splice(index,1);
+      }
+      
+    }
+  }
+  displayInfo(index){
+    this.quoted[index].showInfo = !this.quoted[index].showInfo;
+  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
